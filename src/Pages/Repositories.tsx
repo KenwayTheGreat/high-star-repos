476411027c5047adfda87b8c67afa80a27913fbc
Ahmed Amir Salem (Kenway The Great) <ahmad_amir@live.com>
2021-09-06 06:54:30 +0200

    API integration    API data is fully integrated with the view

--- src/Pages/Repositories.tsx
+++ src/Pages/Repositories.tsx
@@ -1,21 +1,58 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
+import RepositoryCard from '../Components/RepositoryCard';
 
+export interface RepositoryInfo {
+  repoName: string,
+  repoDescription: string,
+  authorName: string,
+  authorAvatar: string,
+  repoStars: number,
+  repoIssues: number,
+  creationDate: string,
+}
 
 function Repositories() {
+  const [repositories, setRepositories] = useState<RepositoryInfo[]>([]);
+
 
   useEffect(() => {
     axios.get(
       `https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc`)
       .then((res) => {
-        console.log(res.data.items);
-        console.log(res.data.items[0].name);
+  
+        res.data.items.map((item: {
+          name: string;
+          description: string;
+          created_at: string;
+          stargazers_count: number;
+          open_issues_count: number;
+          owner: {
+            login: string;
+            avatar_url: string
+          }
+        }) => {
+          setRepositories((repositories) => [...repositories, {
+            repoName: item.name,
+            repoDescription: item.description,
+            creationDate: item.created_at,
+            repoStars: item.stargazers_count,
+            repoIssues: item.open_issues_count,
+            authorName: item.owner.login,
+            authorAvatar: item.owner.avatar_url
+          }]);
+        });
       })
   }, []);
 
   return (
-    <div>
-      testing 12345
+    <div className="row p-5">
+      {console.log(repositories)}
+      {repositories.map((repository, index) => (
+        <RepositoryCard
+          key={index}
+          repository={repository} />
+      ))}
     </div>
   )
 }
