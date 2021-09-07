8d2f8798baa411982b417b24688bde987a62c698
Ahmed Amir Salem (Kenway The Great) <ahmad_amir@live.com>
2021-09-07 12:30:13 +0200

    Design Refinements     Added some styles using bootstrap classname to refine the design.

--- src/Components/RepositoryCard.tsx
+++ src/Components/RepositoryCard.tsx
@@ -1,4 +1,6 @@
 import { RepositoryInfo } from '../Pages/Repositories';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExclamationCircle, faStar } from '@fortawesome/free-solid-svg-icons';
 import Moment from 'react-moment';
 import 'moment-timezone';
 
@@ -9,38 +11,46 @@ interface Props {
 
 function RepositoryCard(props: Props) {
   return (
-    <div className="row p-2 d-flex justify-content-center">
+    <div className="row p-3 d-flex justify-content-center">
       <div className="card col-8 ">
         <div className="row p-3">
           <div className="col-3 d-flex justify-content-center">
-            <img src={props.repository.authorAvatar} width="150 px" height="150 px" />
+            <img
+              className="rounded"
+              src={props.repository.authorAvatar}
+              width="150 px"
+              height="150 px" />
           </div>
-          <div className="col-8">
+          <div className="col-9">
             <div className="row p-1">
-              <div className="col-6">
+              <div className="col-10">
                 <h2>{props.repository.repoName}</h2>
                 <p>{props.repository.repoDescription}</p>
               </div>
             </div>
 
             <div className="row p-1">
-              <div className="col-3">
-                <h6>Stars: {props.repository.repoStars}</h6>
-              </div>
-
-              <div className="col-3">
-                <h6> Issues: {props.repository.repoIssues}</h6>
-              </div>
-
-              <div className="col-6">
-                <h6>
-                  created <Moment fromNow>{props.repository.creationDate}</Moment> by {" "}
-                  {props.repository.authorName}
+              <div
+                className="col-3 text-center">
+                <h6 className="py-2 border border-dark border-2 rounded">
+                  <FontAwesomeIcon icon={faStar} /> {" "}
+                  Stars: {props.repository.repoStars.toLocaleString()}
                 </h6>
               </div>
 
+              <div className="col-3 text-center">
+                <h6 className="py-2 border border-dark border-2 rounded">
+                  <FontAwesomeIcon icon={faExclamationCircle} /> {' '}
+                  Issues: {props.repository.repoIssues.toLocaleString()}
+                </h6>
+              </div>
 
-
+              <div className="col-6">
+                <h6 className="py-2">
+                  Created <Moment fromNow>{props.repository.creationDate}</Moment> by {" "}
+                  <strong>{props.repository.authorName}</strong>
+                </h6>
+              </div>
 
             </div>
 
